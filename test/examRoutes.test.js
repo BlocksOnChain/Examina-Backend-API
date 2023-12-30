ece07a7fde1c7f447b9400c3ee9686b64227cebd
Lruquaf <yavuzselimtuncer02@gmail.com>
2023-12-30 18:05:02 +0300

    new tests added

--- test/examRoutes.test.js
+++ test/examRoutes.test.js
@@ -10,4 +10,19 @@ describe("Exam Routes", () => {
 
 		expect(response.status).to.equal(302);
 	});
+	it("should get all exams", async () => {
+		const response = await supertest(app).get("/exams");
+
+		expect(response.status).to.equal(200);
+		expect(response.body).to.be.an("array");
+	});
+
+	it("should get a specific exam by ID", async () => {
+		const existingExamId = "your-existing-exam-id";
+
+		const response = await supertest(app).get(`/exams/${existingExamId}`);
+
+		expect(response.status).to.equal(200);
+		expect(response.body).to.be.an("object");
+	});
 });
