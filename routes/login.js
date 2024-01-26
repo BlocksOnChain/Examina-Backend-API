3774c6ca7650ff8482b8c0de9f57466df8ecf361
Lruquaf <yavuzselimtuncer02@gmail.com>
2024-01-26 22:11:36 +0300

    login route edited

--- routes/login.js
+++ routes/login.js
@@ -2,24 +2,32 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 
-// Login route
-router.post("/login", (req, res, next) => {
-	passport.authenticate("local", (err, user, info) => {
-		if (err) {
-			console.error(err);
-			return res.status(500).json({ error: err.message });
-		}
-		if (!user) {
-			return res.status(401).json({ error: "Invalid email or password" });
-		}
-		req.logIn(user, (err) => {
+router.post(
+	"/login",
+	(req, res, next) => {
+		passport.authenticate("local", (err, user, info) => {
 			if (err) {
 				console.error(err);
 				return res.status(500).json({ error: err.message });
 			}
-			return res.json({ success: true, user });
-		});
-	})(req, res, next);
-});
+			if (!user) {
+				return res
+					.status(401)
+					.json({ error: "Invalid email or password" });
+			}
+			req.logIn(user, (err) => {
+				if (err) {
+					console.error(err);
+					return res.status(500).json({ error: err.message });
+				}
+				return res.json({ success: true, user });
+			});
+		})(req, res, next);
+	},
+	(req, res) => {
+		// Başarılı giriş durumu
+		res.json({ success: true, user: req.user });
+	}
+);
 
 module.exports = router;
