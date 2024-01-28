0eb2b7f01207f17aeb2ea67dfea67f241b2b6a71
Lruquaf <yavuzselimtuncer02@gmail.com>
2024-01-28 17:09:23 +0300

    login view added

--- routes/login.js
+++ routes/login.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 
+router.get("/login", (req, res) => {
+	res.render("login");
+});
+
 router.post(
 	"/login",
 	(req, res, next) => {
