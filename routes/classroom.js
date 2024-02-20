4d324e3a7fbb6e3024b0d3b15b43d0cfdefc5840
Lruquaf <yavuzselimtuncer02@gmail.com>
2024-02-20 17:30:56 +0300

    classroom get endpoints added

--- routes/classroom.js
+++ routes/classroom.js
@@ -17,4 +17,31 @@ router.post("/", async (req, res) => {
 	}
 });
 
+router.get("/user/:userId", async (req, res) => {
+	try {
+		const userId = req.params.userId;
+		const classrooms = await Classroom.find({ teacher: userId }).populate(
+			"students",
+			"name email"
+		);
+		res.json(classrooms);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ message: "Internal Server Error" });
+	}
+});
+
+router.get("/student/:studentId", async (req, res) => {
+	try {
+		const studentId = req.params.studentId;
+		const classrooms = await Classroom.find({
+			students: studentId,
+		}).populate("teacher", "name email");
+		res.json(classrooms);
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ message: "Internal Server Error" });
+	}
+});
+
 module.exports = router;
