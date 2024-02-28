a6fca504608abad35833a4e9dcb2d076150d7f58
Lruquaf <yavuzselimtuncer02@gmail.com>
2024-02-28 14:29:34 +0300

    new tests added

--- test/classroomRoutes.test.js
+++ test/classroomRoutes.test.js
@@ -17,4 +17,18 @@ describe("Classroom endpoints", () => {
 		expect(res.body).toHaveProperty("_id");
 		classroomId = res.body._id;
 	});
+
+	// GET /classrooms/user/:userId endpoint testi
+	it("should get classrooms of a user", async () => {
+		const res = await request(app).get("/classrooms/user/teacher123");
+		expect(res.statusCode).toEqual(200);
+		expect(res.body.length).toBeGreaterThanOrEqual(1);
+	});
+
+	// GET /classrooms/student/:studentId endpoint testi
+	it("should get classrooms of a student", async () => {
+		const res = await request(app).get("/classrooms/student/student1");
+		expect(res.statusCode).toEqual(200);
+		expect(res.body.length).toBeGreaterThanOrEqual(1);
+	});
 });
