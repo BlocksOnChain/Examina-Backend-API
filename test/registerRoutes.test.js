852c5497afd477d71d14ea97ddba38b894f9bec1
Lruquaf <yavuzselimtuncer02@gmail.com>
2024-03-18 11:21:47 +0300

    register post test created and edited

--- test/registerRoutes.test.js
+++ test/registerRoutes.test.js
@@ -1,7 +1,21 @@
 const request = require("supertest");
 const app = require("../app");
 const mongoose = require("mongoose");
-const connectDB = require("../config/db");
+var Client = require("mina-signer");
+// mainnet or testnet
+const signerClient = new Client({ network: "testnet" });
+
+function signTransaction(privateKey, params) {
+	let signResult;
+	try {
+		// let signClient = getSignClient();
+		let signBody = params.message;
+		signResult = signerClient.signTransaction(signBody, privateKey);
+	} catch (err) {
+		signResult = { message: String(err) };
+	}
+	return signResult;
+}
 
 beforeAll(async () => {
 	mongoose.connect(process.env.MONGO_URI, {});
@@ -26,14 +40,46 @@ describe("API Endpoint Tests", () => {
 		expect(res.body).toHaveProperty("message");
 	});
 
-	// test("POST / should authenticate user and create a new user or find an existing one", async () => {
-	// 	const res = await request(app).post("/").send({
-	// 		walletAddress: "example_wallet_address",
-	// 		signature: "example_signature",
-	// 	});
+	test("POST / should authenticate user and create a new user or find an existing one", async () => {
+		// demo wallet keys
+		const keys_demo = {
+			publicKey:
+				"B62qp9PvZH8zQh9sVr53ApUBrtwt8odZ7hXVXguhq6udpUYQVbRnpVJ",
+			privateKey: "EKFQuaBX6N1r7iijmqDKwvuS9fbVgepfZzryCBcTqcgA7kJQtHW8",
+		};
 
-	// 	expect(res.status).toBe(200);
-	// 	expect(res.body).toHaveProperty("success", true);
-	// 	expect(res.body).toHaveProperty("user");
-	// });
+		// get a session message to verify
+		const resGet = await request(app)
+			.get(
+				"/register/session/get-message-to-sign/B62qp9PvZH8zQh9sVr53ApUBrtwt8odZ7hXVXguhq6udpUYQVbRnpVJ"
+			)
+			.query({
+				walletAddress:
+					"B62qp9PvZH8zQh9sVr53ApUBrtwt8odZ7hXVXguhq6udpUYQVbRnpVJ",
+			});
+
+		// extract the message
+		const message = resGet.body.message;
+		console.log("Message: ", message);
+		// const signMessage_demo =
+		// 	"r90autB62qp9PvZH8zQh9sVr53ApUBrtwt8odZ7hXVXguhq6udpUYQVbRnpVJ";
+
+		// wallet signs the message with private key
+		const signParams = {
+			message: message,
+		};
+		const signRes = signTransaction(keys_demo.privateKey, signParams);
+
+		console.log("Signature: ", signRes);
+
+		// send the message to endpoint to verify
+		const res = await request(app).post("/register").send({
+			walletAddress: keys_demo.publicKey,
+			signature: signRes.signature,
+		});
+
+		expect(res.status).toBe(200);
+		expect(res.body).toHaveProperty("success", true);
+		expect(res.body).toHaveProperty("user");
+	});
 });
