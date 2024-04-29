bf1dd4a09d924c1f8068320a681cead4f6444200
Lruquaf <yavuzselimtuncer02@gmail.com>
2024-04-29 19:39:22 +0300

    refactored endpoints

--- routes/register.js
+++ routes/register.js
@@ -1,113 +1,111 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
-var Client = require("mina-signer");
-// mainnet or testnet
+const Client = require("mina-signer");
+
 const signerClient = new Client({ network: "mainnet" });
 
 router.get("/session/get-message-to-sign/:walletAddress", (req, res) => {
-  const { walletAddress } = req.params;
-  const token = Math.random().toString(36).substring(7);
-  // save token to user's session
-  req.session.token = token;
-  const message = `${req.session.token}${walletAddress}`;
-  req.session.message = message;
-  // console.log("GET req.session.message: ", req.session.message);
-  res.json({ message: message });
+	const { walletAddress } = req.params;
+	const token = Math.random().toString(36).substring(7);
+	req.session.token = token;
+	const message = `${req.session.token}${walletAddress}`;
+	req.session.message = message;
+	res.status(200).json({ message: message });
 });
 
 router.post("/", async (req, res) => {
-  const { walletAddress, signature } = req.body;
-  // console.log("Req.session.message: ", req.session.message);
-  var signture =
-    typeof signature === "string" ? JSON.parse(signature) : signature;
-  const verifyBody = {
-    data: req.session.message,
-    publicKey: walletAddress,
-    signature: signture,
-  };
-  console.log("Data: ", verifyBody.data);
-  console.log("Public Key: ", verifyBody.publicKey);
-  console.log("Signature: ", verifyBody.signature);
+	const { walletAddress, signature } = req.body;
+	const parsedSignature =
+		typeof signature === "string" ? JSON.parse(signature) : signature;
+	const verifyBody = {
+		data: req.session.message,
+		publicKey: walletAddress,
+		signature: parsedSignature,
+	};
 
-  const verifyResult = signerClient.verifyMessage(verifyBody);
-  // console.log("Req.session.token: ", req.session.token);
-  console.log("Verify Result: ", verifyResult);
-  if (verifyResult && req.session.token) {
-    // Create user if not exists
-    try {
-      const user = await User.find({ walletAddress: walletAddress });
-      if (user.length == 0) {
-        const newUser = new User({
-          username: walletAddress,
-          walletAddress,
-        });
-        const saved_user = await newUser.save();
-        console.log("Saved user: ", saved_user);
-        req.session.user = {
-          userId: saved_user._id,
-          walletAddress: saved_user.walletAddress,
-        };
-        return res.json({ success: true, session: req.session.user });
-      } else {
-        req.session.user = {
-          userId: user[0]._id,
-          walletAddress: user[0].walletAddress,
-        };
-        console.log("User already exists: ", user[0]);
-        return res.json({ success: true, session: req.session.user });
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  } else {
-    res.status(401).json({
-      success: false,
-      error: "Invalid signature or didn't have session token",
-    });
-  }
+	const verifyResult = signerClient.verifyMessage(verifyBody);
+
+	if (verifyResult && req.session.token) {
+		try {
+			const user = await User.findOne({ walletAddress: walletAddress });
+			if (!user) {
+				const newUser = new User({
+					username: walletAddress,
+					walletAddress,
+				});
+				const savedUser = await newUser.save();
+				req.session.user = {
+					userId: savedUser._id,
+					walletAddress: savedUser.walletAddress,
+				};
+				return res
+					.status(200)
+					.json({ success: true, session: req.session.user });
+			} else {
+				req.session.user = {
+					userId: user._id,
+					walletAddress: user.walletAddress,
+				};
+				return res
+					.status(200)
+					.json({ success: true, session: req.session.user });
+			}
+		} catch (err) {
+			console.error(err);
+			return res.status(500).json({ error: "Internal server error" });
+		}
+	} else {
+		return res.status(401).json({
+			success: false,
+			error: "Invalid signature or missing session token",
+		});
+	}
 });
 
 if (process.env.NODE_ENV === "development") {
-  router.post("/dev", async (req, res) => {
-    const { walletAddress } = req.body;
-    // Create user if not exists
-    try {
-      const user = await User.find({ walletAddress: walletAddress });
-      if (user.length == 0) {
-        const newUser = new User({
-          username: walletAddress,
-          walletAddress,
-        });
-        const saved_user = await newUser.save();
-        console.log("Saved user: ", saved_user);
-        req.session.user = saved_user._id;
-        return res.json({ success: true, user: req.session.user });
-      } else {
-        req.session.user = user[0]._id;
-        console.log("User already exists: ", user[0]);
-        return res.json({ success: true, user: req.session.user });
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  });
+	router.post("/dev", async (req, res) => {
+		const { walletAddress } = req.body;
+		try {
+			const user = await User.findOne({ walletAddress: walletAddress });
+			if (!user) {
+				const newUser = new User({
+					username: walletAddress,
+					walletAddress,
+				});
+				const savedUser = await newUser.save();
+				req.session.user = savedUser._id;
+				return res
+					.status(200)
+					.json({ success: true, user: req.session.user });
+			} else {
+				req.session.user = user._id;
+				return res
+					.status(200)
+					.json({ success: true, user: req.session.user });
+			}
+		} catch (err) {
+			console.error(err);
+			return res.status(500).json({ error: "Internal server error" });
+		}
+	});
 }
 
 router.get("/session", (req, res) => {
-  if (!req.session.user) {
-    return res.status(401).json({ error: "Not authorized!" });
-  }
-  return res.json({ success: true, session: req.session.user });
+	if (!req.session.user) {
+		return res.status(401).json({ error: "Unauthorized!" });
+	}
+	return res.status(200).json({ success: true, session: req.session.user });
 });
 
 router.get("/logout", (req, res) => {
-  req.session.destroy((err) => {
-    if (err) {
-      return res.status(500).json({ error: "Failed to logout!" });
-    }
-    return res.json({ success: true, message: "Logged out!" });
-  });
+	req.session.destroy((err) => {
+		if (err) {
+			console.error(err);
+			return res.status(500).json({ error: "Failed to logout!" });
+		}
+		return res.status(200).json({ success: true, message: "Logged out!" });
+	});
 });
 
 module.exports = router;
